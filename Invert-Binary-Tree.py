ae8a1e7009e544d114881af2f8191f6be8d6cff3
Xintong Wang <103626688+JAsonT1202@users.noreply.github.com>
2025-04-13 10:30:36 +0900

    [226] [Time Beats: 100.00%] [Memory Beats: 62.58%] - LeetPush

---
+++ Invert-Binary-Tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        \\\
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        \\\
+        def reverse(node):
+            if node is None:
+                return
+            
+            tmp = node.left
+            node.left = node.right
+            node.right = tmp
+        
+            reverse(node.left)
+            reverse(node.right)
+        
+        reverse(root)
+
+        result = []
+
+        def order(node):
+            if node is None:
+                return
+            
+            result.append(node.val)
+            order(node.right)
+            order(node.left)
+        
+        order(root)
+        
+        return root
+
+
+        
